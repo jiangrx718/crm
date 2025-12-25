3e7230afd632e47e652cd43eed4b300076eed74a
ruoxi <ruoxi@163.com>
2025-12-25 17:32:05 +0800

    修改

--- internal/model/crm_role.go
+++ internal/model/crm_role.go
@@ -20,7 +20,7 @@ func (r *CRMRole) TableName() string {
 
 // CRMRolePermission 角色权限表
 type CRMRolePermission struct {
-	Id           int       `gorm:"column:id;type:int;not null;primaryKey;autoIncrement;comment:自增主键;" json:"id"`
+	Id           int       `gorm:"column:id;type:bigint;not null;primaryKey;autoIncrement;comment:自增主键;" json:"id"`
 	RoleId       string    `gorm:"column:role_id;type:char(36);not null;default:'';index:idx_role_id;comment:角色ID;" json:"role_id"`
 	PermissionId string    `gorm:"column:permission_id;type:char(36);not null;default:'';comment:权限ID" json:"permission_id"`
 	CreatedAt    time.Time `gorm:"column:created_at;type:timestamp;not null;default:CURRENT_TIMESTAMP;index:idx_created_at;comment:创建时间" json:"created_at"`
